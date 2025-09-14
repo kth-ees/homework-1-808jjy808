2bfcecf6ea6fdff7e28076007aaf5fdc720d3b72
reticent <gjy0508082021@163.com>
2025-09-14 17:08:43 +0200

    finish q4 count_1

--- q4/count_1_tb.sv
+++ q4/count_1_tb.sv
@@ -7,4 +7,25 @@ module count_1_tb;
   logic [2:0] out;
 
   // complete
+
+count_1 UUT (
+    .a(a),
+    .out(out)
+  );
+
+  initial begin
+    a = 4'b0000;
+    #10;
+    for(int i=0; i<16; i++) begin
+      a = $urandom_range(0, 15);
+      #1;
+        $display("a=%0d -> out=%0d", a, out);
+      end
+    #20;
+
+    $finish;
+  end
+
+
+
 endmodule
