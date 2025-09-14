e703fdf5bc9dac5661825c002de54ce0be1458c2
reticent <gjy0508082021@163.com>
2025-09-14 17:52:11 +0200

    finish q5 bin2gray

--- q5/bin2gray_tb.sv
+++ q5/bin2gray_tb.sv
@@ -7,4 +7,25 @@ module bin2gray_tb;
   logic [3:0] gray;
 
   // complete
+
+bin2gray UUT (
+    .binary(binary),
+    .gray(gray)
+  );
+
+  initial begin
+    binary = 4'b0000;
+    #10;
+    for(int i=0; i<16; i++) begin
+      binary = $urandom_range(0, 15);
+      #1;
+        $display("binary=%0d -> gray=%0d", binary, gray);
+      end
+    #20;
+
+    $finish;
+  end
+
+
+
 endmodule
