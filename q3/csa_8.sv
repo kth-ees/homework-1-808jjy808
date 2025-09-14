c2275618397a11318fa11f426ae2c34754a0d3d6
reticent <gjy0508082021@163.com>
2025-09-14 16:50:24 +0200

    finish q3 csa_8

--- q3/csa_8.sv
+++ q3/csa_8.sv
@@ -5,5 +5,40 @@ module csa_8 (
 );
   // …
   // Add your description here
+
+
+logic carry_low;
+logic [3:0] sum_high_no_carry, sum_high_with_carry;
+logic carry_high_no_carry, carry_high_with_carry;
+logic [3:0] b_high_with_carry;
+
+adder_4 adder_low (
+    .A(a[3:0]),
+    .B(b[3:0]),
+    .sum(sum[3:0]),
+    .carry(carry_low)
+  );
+
+adder_4 adder_high_0 (
+    .A(a[7:4]),
+    .B(b[7:4]),
+    .sum(sum_high_no_carry),
+    .carry(carry_high_no_carry)
+  );
+
+assign b_high_with_carry = b[7:4] + 1;
+adder_4 adder_high_1 (
+    .A(a[7:4]),
+    .B(b_high_with_carry), 
+    .sum(sum_high_with_carry),
+    .carry(carry_high_with_carry)
+  );
+
+assign sum[7:4] = carry_low ? sum_high_with_carry : sum_high_no_carry;
+assign carry    = carry_low ? carry_high_with_carry : carry_high_no_carry;
+
+
+
+
   // …
 endmodule
--- q3/csa_8_tb.sv
+++ q3/csa_8_tb.sv
@@ -3,9 +3,34 @@ module csa_8_tb;
   // Inputs
   logic [7:0] a;
   logic [7:0] b;
-  logic [7:0] sum,
+  logic [7:0] sum;
   logic carry;
 
   // complete
+
+  csa_8 UUT (
+    .a(a),
+    .b(b),
+    .sum(sum),
+    .carry(carry)
+  );
+
+  initial begin
+
+for(int i=0; i<16; i++) begin
+      a = $urandom_range(0, 255);
+      b = $urandom_range(0, 255);
+      #10;
+        $display("a=%0d, b=%0d -> sum=%0d, carry=%b", a, b, sum, carry);
+      end
+    #20;
+
+    $finish;
+  end 
+
+
+
+
+
   
 endmodule
