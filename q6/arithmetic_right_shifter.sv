178098cb577ea5f5d97b60b652af73c127dcb74d
reticent <gjy0508082021@163.com>
2025-09-14 18:30:20 +0200

    finish q6 arithmetic_right_shifter

--- q6/arithmetic_right_shifter.sv
+++ q6/arithmetic_right_shifter.sv
@@ -5,4 +5,20 @@ module arithmetic_right_shifter #(parameter N) (
 );
 
   // complete the module
+
+logic sign_bit;
+assign sign_bit = input_data[N-1];
+
+always_comb begin
+  case(control)
+    2'b00: shifted_result = input_data; 
+    2'b01: shifted_result = {sign_bit, input_data[N-1:1]}; 
+    2'b10: shifted_result = {sign_bit, sign_bit, input_data[N-1:2]}; 
+    2'b11: shifted_result = {sign_bit, sign_bit, sign_bit, input_data[N-1:3]}; 
+
+  endcase
+end
+
+
+
 endmodule
--- q6/arithmetic_right_shifter_tb.sv
+++ q6/arithmetic_right_shifter_tb.sv
@@ -2,6 +2,35 @@ module arithmetic_right_shifter_tb;
 
   localparam N = 8;
 
+
+
+    // Inputs
+  logic [N-1:0] input_data;
+  logic [1:0] control;
+  // Outputs
+  logic [N-1:0] shifted_result;
+
+
   // complete
-  // Inputs
+arithmetic_right_shifter #(N) UUT (
+    .input_data(input_data),
+    .control(control),
+    .shifted_result(shifted_result)
+  );
+
+
+  initial begin
+      input_data = 8'b10101010;
+      control = 2'b00;
+      #10;
+    for(int i=0; i<20; i++) begin
+      input_data = $urandom_range(0, 255);
+      control = $urandom_range(0, 3);
+      #2;
+      $display("input_data=%b, control=%b -> shifted_result=%b", input_data, control, shifted_result);
+    end
+    #20;
+    
+    $finish;
+  end
 endmodule
